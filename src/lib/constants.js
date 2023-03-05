e91af20d1b0d4fc06897557523cdd83a8aac6cb8
Ryan Sandoval <rsandova@ualberta.ca>
2023-03-05 02:16:43 -0800

    Update constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,10 +4,10 @@ export const PLAYER_SERVER_PREFIX = "pserv";
 export const HACK_TARGETS_LIST = 'targetList.txt';
 
 // Server Ranking Config
-export const MAX_MONEY_WEIGHT = 2.5;
-export const MIN_SECURITY_WEIGHT = -1.2;
+export const MAX_MONEY_WEIGHT = 1.5;
+export const MIN_SECURITY_WEIGHT = -1.0;
 export const GROWTH_WEIGHT = 1.5;
-export const CYCLE_TIME_WEIGHT = -0.9;
+export const CYCLE_TIME_WEIGHT = -1.8;
 
 // SCP Scripts
 export const WEAKEN_SCRIPT = '/lib/weaken.js';
