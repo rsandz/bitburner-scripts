9757ac881d78397fa602ccac3f618b50777b4d6b
Ryan Sandoval <rsandova@ualberta.ca>
2023-03-05 05:00:26 -0800

    FIx publisher not publishing any data

--- src/utils/startPublishingManager.js
+++ src/utils/startPublishingManager.js
@@ -1,3 +1,4 @@
+import { HACK_TARGETS_LIST } from 'lib/constants';
 import { PublishingManager } from 'lib/publishing';
 
 /**
@@ -5,6 +6,6 @@ import { PublishingManager } from 'lib/publishing';
 */
 export async function main(ns) {
     ns.disableLog("ALL");
-    const manager = new PublishingManager(ns);
+    const manager = new PublishingManager(ns, JSON.parse(ns.read(HACK_TARGETS_LIST)));
     await manager.run();
 }
